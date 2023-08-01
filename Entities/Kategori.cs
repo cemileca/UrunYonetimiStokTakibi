80cd6c0905363c607c3d0a52ae19fa9b27d5a3fe
Güzel İşler <97739544+cemileca@users.noreply.github.com>
2023-08-01 09:41:18 +0300

    01.08.2023 09:41

---
+++ Entities/Kategori.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities
+{
+    public class Kategori:IEntity 
+    {
+        public int Id { get; set; }
+        public string KategoriAdi { get; set; }
+        public string KategoriAciklamasi { get; set; }
+        public bool Aktif { get; set; }
+    }
+}
---
+++ Entities/Marka.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Marka:IEntity
+    {
+        public int Id { get; set; }
+        public string MarkaAdi { get; set; }
+        public string MarkaAciklamasi { get; set; }
+        public bool Aktif { get; set; }
+    }
+}
